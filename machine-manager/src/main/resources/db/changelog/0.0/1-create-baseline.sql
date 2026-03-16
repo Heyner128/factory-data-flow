2088289fe27c9efba20e9a1cdea46c0f567b971b
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2026-03-16 19:48:00 +0100

    rename `id` columns to `uuid` in database schema to improve clarity

--- machine-manager/src/main/resources/db/changelog/0.0/1-create-baseline.sql
+++ machine-manager/src/main/resources/db/changelog/0.0/1-create-baseline.sql
@@ -1,9 +1,9 @@
 CREATE TABLE manufacturing_line(
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     name TEXT
 );
 
 CREATE TABLE machine(
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     name TEXT
 );
